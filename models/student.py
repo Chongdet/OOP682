601882eaf1e5d3d09a9af30b85c5752d340b03e9
Chongdet.ja.68@ubu.ac.th <chongdet.ja.68@ubu.ac.th>
2026-01-30 15:31:45 +0700

    บนทกงาน Week 09: เรอง OOP Inheritance

---
+++ models/student.py
@@ -0,0 +1,10 @@
+from models.person import Person
+
+
+class Student(Person):
+    def __init__(self, pid, name, age, student_id):
+        super().__init__(pid, name, age)
+        self.student_id = student_id
+
+    def info(self):  # Overriding
+        return f"[Student] {self.name}, ID: {self.student_id}"
